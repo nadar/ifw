814d775450e1c66a4c21cbf544e36d986f49059a
Nadar <git@nadar.io>
2015-03-30 23:00:53 +0200

    added database class component.

--- framework/components/Db.php
+++ framework/components/Db.php
@@ -13,6 +13,13 @@ class Db extends \ifw\core\Component
     
     private $_dbo = null;
     
+    private $_stmt = null;
+    
+    public function init()
+    {
+        $this->setConnection();
+    }
+    
     public function setConnection()
     {
         try {
@@ -24,18 +31,40 @@ class Db extends \ifw\core\Component
     
     public function query($statement, array $params = [])
     {
-        $query = $this->_dbo->prepare($statement);
+        $this->_stmt = null;
+        $this->_stmt = $this->_dbo->prepare($statement);
         foreach($params as $key => $value) {
             $query->bindParam($key, $value);
         }
-        $retun = $query->execute();
-        $query->closeCursor();
-        $query = null;
-        return $return;
+        $this->_stmt->execute();
+        return $this;
+    }
+    
+    public function getStatement()
+    {
+        return $this->_stmt;
+    }
+    
+    public function fetch($fetchMode = \PDO::FETCH_ASSOC)
+    {
+        return $this->_stmt->fetch($fetchMode);
+    }
+    
+    public function fetchAll($fetchMode = \PDO::FETCH_ASSOC)
+    {
+        $response = $this->_stmt->fetchAll($fetchMode);
+        $this->_stmt->closeCursor();
+        $this->_stmt = null;
+        return $response;
     }
     
     public function closeConnection()
     {
         $this->_dbo = null;
+    }
+    
+    public function __destruct()
+    {
+        $this->closeConnection();
     }
 }
