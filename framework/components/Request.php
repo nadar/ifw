3348e8cb030d11dd9aeb1e99545c0d74b87319aa
Nadar <git@nadar.io>
2015-04-18 23:53:53 +0200

    added getData, dataTypes, added post data

--- framework/components/Request.php
+++ framework/components/Request.php
@@ -3,18 +3,57 @@ namespace ifw\components;
 
 class Request extends \ifw\core\Component
 {
+    const TYPE_GET = 'GET';
+    
+    const TYPE_POST = 'POST';
+    
+    private $_dataTypes = [self::TYPE_GET, self::TYPE_POST];
+    
     private $_get = null;
     
-    public function get($varName, $defaultValue = false)
+    private $_post = null;
+    
+    private function ensureType($dataType)
     {
-        if (is_null($this->_get)) {
-            $this->_get = $_GET;
+        $dataType = strtoupper($dataType);
+        if (!in_array($dataType, $this->_dataTypes)) {
+            throw new \ifw\core\Exception("The dataType $dataType does not exists.");
         }
         
-        if (!array_key_exists($varName, $this->_get)) {
-             return $defaultValue;
+        return $dataType;
+    }
+    
+    public function get($varName, $defaultValue = null)
+    {
+        $data = $this->getData(self::TYPE_GET);
+        if (!array_key_exists($varName, $data)) {
+            return $defaultValue;
         }
-        
-        return $this->_get[$varName];
+        return $data[$varName];
+    }
+    
+    public function post($varName, $defaultValue = null)
+    {
+        $data = $this->getData(self::TYPE_POST);
+        if (!array_key_exists($varName, $data)) {
+            return $defaultValue;
+        }
+        return $data[$varName];
+    }
+    
+    public function getData($dataType)
+    {
+        $dataType = $this->ensureType($dataType);
+        switch($dataType) {
+            case self::TYPE_GET:
+                return (is_null($this->_get)) ? $this->_get = $_GET : $this->_get;
+                break;
+            case self::TYPE_POST:
+                return (is_null($this->_post)) ? $this->_post = $_POST : $this->_post;
+                break;
+            default:
+                throw new \ifw\core\Exception("the dataType $dataType does not exists!");
+                break;
+        }
     }
 }
