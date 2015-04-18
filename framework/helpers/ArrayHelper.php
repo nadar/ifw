71d308e57153b5e6c59af7e929afb15199c41763
Nadar <git@nadar.io>
2015-04-19 00:00:50 +0200

    added array helper

---
+++ framework/helpers/ArrayHelper.php
@@ -0,0 +1,10 @@
+<?php
+namespace ifw\helpers;
+
+class ArrayHelper {
+    
+    public static function toObject(array $array)
+    {
+        return json_decode(json_encode($array), FALSE);
+    }
+}
