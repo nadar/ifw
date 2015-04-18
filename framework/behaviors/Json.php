7157699114ca1984da51301e8c35c848fdf78ace
Nadar <git@nadar.io>
2015-04-18 21:05:15 +0200

    added test method in json behavior

--- framework/behaviors/Json.php
+++ framework/behaviors/Json.php
@@ -6,17 +6,22 @@ class Json extends \ifw\core\Behavior
 {
     public function init()
     {
-        $this->context->on('EVENT_AFTER_ACTION', [$this, 'encode']);
+        $this->context->on('EVENT_AFTER_ACTION', [$this, 'transform']);
     }
     
-    public function encode()
+    public function transform()
     {
         $content = $this->context->response;
         if (!is_string($content) && (is_array($content) || is_object($content))) {
-            $this->context->response = json_encode($content);
+            $this->context->response = $this->encode($content);
             return true;
         }
         
         throw new \Exception('Invalid action response. The provided response must be an array or object.');
+    }
+    
+    public function encode($content)
+    {
+        return json_encode($content);
     }
 }
