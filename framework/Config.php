31bfa32fda66a26aa79e19aefe8c2ebb384e22d2
Nadar <git@nadar.io>
2015-04-25 23:43:07 +0200

    added config builder class

---
+++ framework/Config.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace ifw;
+
+/**
+ * 
+ * The below example config
+ * ```
+ * [
+ *      'basePath' => dirname(__DIR__),
+ *      'defaultModule' => 'test',
+ *      'components' => [
+ *          'db' => [
+ *              'dsn' => 'mysql:host=localhost;dbname=DATABASE',
+ *              'user' => 'USER',
+ *              'password' => 'PASSWORD',
+ *          ]
+ *      ],
+ *      'modules' => [
+ *          'test' => [
+ *              'class' => '\\app\\modules\\test\\Module',
+ *          ],
+ *      ],
+ * ]
+ * ```
+ * can be builded with the Config class like this:
+ * ```
+ * $config = new \ifw\Config(dirname(__DIR__), 'test');
+ * $config->module('test', '\\app\\modules\\test\\Module');
+ * $config->component('db', ['dsn' => 'mysql:host=localhost;dbname=DATABASE', 'user' => 'USER', 'password' => 'PASSWORD']);
+ * $config->get(); // returns the array
+ * ```
+ */
+class Config
+{
+    public $config = [
+        'basePath' => null,
+        'defaultModule' => null,
+        'modules' => [],
+        'components' => [],
+    ];
+    
+    public function __construct($basePath, $defaultModule)
+    {
+        $this->config = [
+            'basePath' => $basePath,
+            'defaultModule' => $defaultModule  
+        ];
+    }
+    
+    public function module($name, $className, array $args = [])
+    {
+        if ($this->has("modules.$name")) {
+            throw new \Exception("the module does already exist.");
+        }
+        $objectConfig = $args;
+        $objectConfig['class'] = $className;
+        $this->config['modules'][$name] = $objectConfig;
+    }
+    
+    public function component($name, array $args = [], $className = null)
+    {
+        if ($this->has("components.$name")) {
+            throw new \Exception("the component does already exist.");
+        }
+        
+        $objectConfig = $args;
+        if ($className !== null) {
+            $objectConfig['class'] = $className;
+        }
+        $this->config['components'][$name] = $objectConfig;
+    }
+    
+    public function has($key)
+    {
+        $keys = explode(".", $key);
+        $array = $this->config;
+        $i = 0;
+        foreach ($keys as $name) {
+            if (array_key_exists($name, $array)) {
+                $array = $array[$name];
+            } else {
+                return false;
+            }
+            $i++;
+        }
+        
+        return true;
+    }
+    
+    public function get()
+    {
+        return $this->config;
+    }
+    
+}
