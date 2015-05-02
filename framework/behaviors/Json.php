f5ede335002cfa0b68a6688f75a37534c7bc16d6
Nadar <git@nadar.io>
2015-05-03 00:13:24 +0200

    json behavior added response contentType

--- framework/behaviors/Json.php
+++ framework/behaviors/Json.php
@@ -2,6 +2,8 @@
 
 namespace ifw\behaviors;
 
+use Ifw;
+
 class Json extends \ifw\core\Behavior
 {
     public function init()
@@ -13,6 +15,7 @@ class Json extends \ifw\core\Behavior
     {
         $content = $this->context->response;
         if (!is_string($content) && (is_array($content) || is_object($content))) {
+            Ifw::$app->response->setContentType('json');
             $this->context->response = $this->encode($content);
             return true;
         }
