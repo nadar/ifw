5c30cfe2bc5ca6e2a149f5dcc128898f24c9270c
Nadar <git@nadar.io>
2015-05-03 20:32:22 +0200

    added file helper with recursive copy function

---
+++ framework/helpers/FileHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ifw\helpers;
+
+class FileHelper
+{
+    public static function copyr($source, $destination, array $fileTypeFilter = ["css", "js", "png", "jpg", "gif", "less"])
+    { 
+        $dir = opendir($source); 
+        @mkdir($destination); 
+        while(false !== ( $file = readdir($dir)) ) { 
+            if (( $file != '.' ) && ( $file != '..' )) { 
+                if ( is_dir($source . DIRECTORY_SEPARATOR . $file) ) { 
+                    static::copyr($source . DIRECTORY_SEPARATOR . $file,$destination . DIRECTORY_SEPARATOR . $file); 
+                } 
+                else { 
+                    $fileType = pathinfo($source . DIRECTORY_SEPARATOR . $file, PATHINFO_EXTENSION);
+                    if (in_array(strtolower($fileType), $fileTypeFilter)) {
+                        copy($source . DIRECTORY_SEPARATOR . $file,$destination . DIRECTORY_SEPARATOR . $file);
+                    }   
+                } 
+            } 
+        } 
+        closedir($dir); 
+    }  
+}
