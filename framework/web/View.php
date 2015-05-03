6b062fd969cf95153a48894ad8be12bbe8895525
Nadar <git@nadar.io>
2015-05-03 20:33:44 +0200

    asset view integration

--- framework/web/View.php
+++ framework/web/View.php
@@ -1,8 +1,14 @@
 <?php
 namespace ifw\web;
 
+use Ifw;
+
 class View extends \ifw\core\Component
 {
+    protected $js = [];
+    
+    protected $css = [];
+    
     public function renderFile($viewFile, array $params = [])
     {
         ob_start();
@@ -24,6 +30,24 @@ class View extends \ifw\core\Component
         return $view;
     }
     
+    public function endBody()
+    {
+        foreach($this->js as $key => $value) {
+            $this->js[$key] = '<script src="'.$value.'"></script>';
+        }
+        
+        return implode("\n\r", $this->js);
+    }
+    
+    public function head()
+    {
+        foreach($this->css as $key => $value) {
+            $this->css[$key] = '<link href="'.$value.'" rel="stylesheet" />';
+        }
+        
+        return implode("\n\r", $this->css);
+    }
+    
     public function render($viewFile, array $params = [], $context)
     {
         // @todo change to array merger helper function
@@ -41,5 +65,13 @@ class View extends \ifw\core\Component
         return $this->renderFile($this->findViewFile($layoutFile, $context->getLayoutPath()), [
             "content" => $this->renderFile($this->findViewFile($viewFile, $context->getViewPath()), $_vars)
         ], $context);
+    }
+    
+    public function registerAsset($assetClass)
+    {
+        $object = Ifw::createObject($assetClass, ['view' => $this]);
+        $this->css = array_merge($this->css, $object->getCss());
+        $this->js = array_merge($this->js, $object->getJs());
+        return $object;
     }
 }
