7ca425dbb21eb57b5434cf95cc35322496091435
Nadar <git@nadar.io>
2015-05-09 11:56:10 +0200

    added ifw module bootstrap controllerMap,

--- framework/module/IfwModule.php
+++ framework/module/IfwModule.php
@@ -4,5 +4,8 @@ namespace ifw\module;
 
 class IfwModule extends \ifw\core\Module
 {
-    
+    public static function bootstrap($app)
+    {
+        $app->addControllerMap('migration', ['module' => 'ifw', 'controller' => 'migration']);
+    }
 }
