f8da149023d2042d71910b3824f3782ba46adb62
Nadar <git@nadar.io>
2015-05-10 22:07:37 +0200

    cli prompt

--- framework/cli/Controller.php
+++ framework/cli/Controller.php
@@ -2,6 +2,18 @@
 
 namespace ifw\cli;
 
+/**
+ * http://php.net/manual/en/features.commandline.php#73765 colors
+ * http://php.net/manual/en/features.commandline.php#94924 prompt
+ * @author nadar
+ */
 abstract class Controller extends \ifw\core\Controller
 {
+    protected function prompt($text)
+    {
+        echo $text;
+        $handle = fopen ("php://stdin","r");
+        $line = fgets($handle);
+        return $line;
+    }
 }
