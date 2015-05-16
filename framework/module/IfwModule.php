54cfc32b6827f7b9a2f44a8f74aa59680083f7ef
Nadar <git@nadar.io>
2015-05-16 02:10:52 +0200

    migrations test

--- framework/module/IfwModule.php
+++ framework/module/IfwModule.php
@@ -6,6 +6,7 @@ class IfwModule extends \ifw\core\Module
 {
     public static function bootstrap($app)
     {
+        $app->addControllerMap('help', ['module' => 'ifw', 'controller' => 'help']);
         $app->addControllerMap('migration', ['module' => 'ifw', 'controller' => 'migration']);
     }
 }
--- framework/module/clis/MigrationController.php
+++ framework/module/clis/MigrationController.php
@@ -2,10 +2,31 @@
 
 namespace ifw\module\clis;
 
+use Ifw;
+
 class MigrationController extends \ifw\cli\Controller
 {
     public function actionIndex()
     {
-        return 'migrations';
+        foreach(Ifw::$app->getModules() as $id => $module) {
+            $module = Ifw::$app->getModule($id);
+            //print_r($module);
+            
+            $files = \ifw\helpers\FileHelper::readdir($module->getBasePath() . DIRECTORY_SEPARATOR . 'tables');
+            
+            foreach($files as $file) {
+                echo "compare: " . $file['file']. PHP_EOL;
+                $class = pathinfo($file['name'], PATHINFO_FILENAME);
+                
+                try {
+                    require_once($file['file']);
+                    $obj = new $class();
+                    $obj->compare();
+                    
+                } catch (\Exception $e) {
+                    echo "error:" . $e->getMessage() . PHP_EOL;    
+                }
+            }
+        }
     }
 }
