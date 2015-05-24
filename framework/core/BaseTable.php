00b9c7a0a8d4eeb5a35302e6164207e6a2eab00a
Nadar <git@nadar.io>
2015-05-24 23:01:52 +0200

    migration create table

--- framework/core/BaseTable.php
+++ framework/core/BaseTable.php
@@ -3,6 +3,7 @@
 namespace ifw\core;
 
 use Ifw;
+use Pdo;
 
 abstract class BaseTable extends \ifw\core\Object
 {
@@ -16,7 +17,7 @@ abstract class BaseTable extends \ifw\core\Object
     
     /**
      * return [
-     *       'id' => ['int' => 11, 'default' => 0],
+     *       'id' => ['int' => 11, 'default 0'],
      *       'firstname' => ['varchar' => 200, 'NOT NULL'],
      *       'lastname' => ['varchar' => 120, 'NOT NULL'],
      *       'mail' => ['varchar' => 80],
@@ -52,11 +53,24 @@ abstract class BaseTable extends \ifw\core\Object
     
     private function queryCreateTable()
     {
-        $sql = 'CREATE TABLE `' . $this->name() . '` (';
+        $imp = [];
         foreach ($this->fields() as $fieldName => $opts) {
+            $part = [];
+            $part[] = $fieldName;
+            foreach($opts as $k => $v) {
+                if (is_int($k)) { // not field type
+                    $part[] = $v;
+                } else {
+                    $part[] = "$k($v)";
+                }
+            }
             
+            $imp[] = implode(" ", $part);
         }
-        $sql.= ')';
+        foreach($this->pk() as $key) {
+            $imp[] = 'PRIMARY KEY (' . implode(",", $this->pk()) . ')';
+        }
+        $sql = 'CREATE TABLE `' . $this->name() . '` (' .implode(', ', $imp).')';
         
         return $sql;
     }
@@ -68,9 +82,23 @@ abstract class BaseTable extends \ifw\core\Object
         if (count($shema) == 0) {
             $sql = $this->queryCreateTable();
             var_dump($sql);
-            
+            try {
+                $db = Ifw::$app->db->getConnection();
+                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $r = $db->exec($sql);
+                var_dump($r);
+                
+            } catch (Exception $e) {
+                var_dump($e->getMessage);
+            }
+            /*
+            $response = $this->db()->query($sql)->getStatement();
+            var_dump($response);
+            */
             //$exec = $this->db()->query);
             //var_dump($exec);
+        } else {
+            echo "TABLE EXISTS... compare";
         }
     }
 }
